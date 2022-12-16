b66d18f4d6962899c905d97fdbae2cdc9678929e
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-12-16 20:17:13 +0800

    Add files via upload

---
+++ menghitung waktu.py
@@ -0,0 +1,7 @@
+print('''MENGHITUNG WAKTU YANG DI PERLUKAN UNTUK MENEMPUH SUATU TUJUAN
+''')
+
+j = int(input("Masukkan Jarak(KM) : "))
+k = int(input("Masukkan Kecepatan(KM/JAM) : "))
+w = j / k
+print(f"Waktu yang dibutuhkan untuk menepuh jarak sejauh {j} KM\ndengan kecepatan {k}KM/JAM adalah {w} Jam")
